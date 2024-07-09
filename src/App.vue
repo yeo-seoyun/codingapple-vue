9f7e9214797915c293466c670cc2458d129d9886
yeoseoyun <yeosummer94@gmail.com>
2024-07-09 21:26:20 +0900

    함수 만들어서 사용하기 this.함수명

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     <h4>{{ products[0] }}</h4>
     <p>60 만원</p>
       <!-- v-on:click="" -->
-    <button @:click="신고수++">허위매물신고</button> <span>신고수 : {{ 신고수 }}</span>
+    <button @:click="increase">허위매물신고</button> <span>신고수 : {{ 신고수 }}</span>
     <!-- 변수에 ++ (1씩 증가) -->
     <!-- Vue는 데이터가 변하면 HTML은 자동으로 변경됨 -->
   </div>
@@ -34,6 +34,13 @@ export default {
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
+  },
+  // Vue에서 함수 만드는 방법
+  // Vue에서 함수 만들때 주의사항 -> 함수안에서 데이터를 불러올 때에는 this.데이터명
+  methods : {
+    increase(){
+      this.신고수 += 1;
+    },
   },
   components: {
 
