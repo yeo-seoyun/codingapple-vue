551198f58f5872167d5b828ecc69dd3bf5438100
yeoseoyun <yeosummer94@gmail.com>
2024-07-09 21:13:53 +0900

    v-on 핸들이벤트

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,26 @@
 
   <div class="menu">
     <a v-for="(상단메뉴,i) in 메뉴들" :key="i">{{ 상단메뉴 }}</a>
-    <!-- <a v-for="상단메뉴 in 메뉴들" :key="상단메뉴">{{ 상단메뉴 }}</a> -->
   </div>
 
-  <!-- Vue의 HTML 반복문 <태그 v-for="작명 in 횟수 :key="작명"> -->
-  <!-- Vue 반복문 특징
-    1. array/object 집어넣기 가능
-    -> 자료안의 데이터 갯수만큼 반복
-    -> 작명한 변수는 데이터안의 자료가 됨
-    2. 변수 작명 2개까지 가능
-    -> 왼쪽 변수는 array내의 데이터
-    -> 오른쪽 변수는 1씩 증가하는 정수
-    -->
-
-  <div v-for="(상가,i) in products" :key="i">
-    <h4>{{ 상가 }}</h4>
-    <!-- <h4>{{ products[i] }}</h4> -->
+  <div>
+    <h4>{{ products[0] }}</h4>
     <p>60 만원</p>
+      <!-- v-on:click="" -->
+    <button @:click="신고수++">허위매물신고</button> <span>신고수 : {{ 신고수 }}</span>
+    <!-- 변수에 ++ (1씩 증가) -->
+    <!-- Vue는 데이터가 변하면 HTML은 자동으로 변경됨 -->
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>70 만원</p>
   </div>
 
 </template>
-
-<!-- {{데이터바인딩}}을 하는 이유
-  1. HTML에 하드코딩을 하면 추후 변경이 어려움
-  2. Vue의 실시간 자동 렌더링 기능 사용 
-  -> Vue는 data를 변경하면 data와 관련된 HTML도 실시간으로 변경 됨 
-  = 웹앱 사이트 만들 수 있음
--->
 
 <script>
 
@@ -37,6 +30,7 @@ export default {
   name: 'App',
   data(){
     return {
+      신고수 : 0,
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
