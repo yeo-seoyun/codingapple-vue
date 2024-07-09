93f28ecd63b3f1aa82f240ee9bab26cec73126cc
yeoseoyun <yeosummer94@gmail.com>
2024-07-09 19:29:37 +0900

    vue 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4>xx 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>xx 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+
 </template>
 
+<!-- {{데이터바인딩}}을 하는 이유
+ 1. HTML에 하드코딩을 하면 추후 변경이 어려움
+-->
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data(){
+    return {
+      price1 : 60,
+      price2 : 70,
+    }
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
