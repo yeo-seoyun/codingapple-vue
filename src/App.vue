dc315dec51c11060a6f87959609f9fe8c55472fd
yeoseoyun <yeosummer94@gmail.com>
2024-07-11 17:46:47 +0900

    export import 문법 사용해서 데이터 불러오기

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,6 @@
       <button @:click="모달창열림 = false">닫기</button>
     </div>
   </div>
-  <!-- 동적인 UI 만드는 법
-    0. 화면 디자인 
-    1. UI의 현재 상태를 데이터로 저장
-    2. 데이터에 따라 UI가 어떻게 보일지 작성  
-
-    v-if="조건식" -> 조건식이 true일 때만 HTML을 보여줌
-  -->
 
   <div class="menu">
     <a v-for="(상단메뉴,i) in 메뉴들" :key="i">{{ 상단메뉴 }}</a>
@@ -24,42 +17,25 @@
     <img src="./assets/room0.jpg" alt="room0" class="room-img ">
     <h4 @:click="모달창열림 = true">{{ products[0] }}</h4>
     <p>60 만원</p>
-      <!-- v-on:click="" -->
     <button @:click="신고수[0]++">허위매물신고</button> <span>신고수 : {{ 신고수[0] }}</span>
-    <!-- 변수에 ++ (1씩 증가) -->
-    <!-- Vue는 데이터가 변하면 HTML은 자동으로 변경됨 -->
-  </div>
-  <div>
-    <img src="./assets/room1.jpg" alt="room1" class="room-img ">
-    <h4>{{ products[1] }}</h4>
-    <p>50 만원</p>
-    <button @:click="신고수[1]++">허위매물신고</button> <span>신고수 : {{ 신고수[1] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room2.jpg" alt="room2" class="room-img ">
-    <h4>{{ products[2] }}</h4>
-    <p>70 만원</p>
-    <button @:click="신고수[2]++">허위매물신고</button> <span>신고수 : {{ 신고수[2] }}</span>
   </div>
 
 </template>
 
 <script>
 
+import rooms from './assets/rooms.js'
 
 export default {
   name: 'App',
   data(){
     return {
-      // 현재 UI가 어떻게 보일지 (현재상태)?
       모달창열림 : false, 
       신고수 : [0, 0, 0],
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
-  // Vue에서 함수 만드는 방법
-  // Vue에서 함수 만들때 주의사항 -> 함수안에서 데이터를 불러올 때에는 this.데이터명
   methods : {
     // increase(){
     //   this.신고수[0] += 1;
