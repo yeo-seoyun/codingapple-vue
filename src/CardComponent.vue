18c3013884489a8df9ea2e78dee195099b9d1146
yeoseoyun <yeosummer94@gmail.com>
2024-07-15 21:44:17 +0900

    custom event @click="$emit('작명')

--- src/CardComponent.vue
+++ src/CardComponent.vue
@@ -1,11 +1,16 @@
 <template lang="">
   <div>
     <img :src="원룸.image" alt="room0" class="room-img ">
-    <h4 @:click="모달창열림 = true; 클릭 = i">{{ 원룸.title }}</h4>
+    <h4 @click="$emit('openModal')" >{{ 원룸.title }}</h4>
+    <!-- custom event
+      부모가 가진 데이터를 바꾸고 싶으면 자식컴포넌트는 부모에게 메세지를 주어야한다.
+      @click="$emit('작명') -> 부모 컴포넌트에게 전달하기
+    -->
     <p>{{ 원룸.price }}원</p>
   </div>
 </template>
 <script>
+// props 받아 온 데이터는 변경 불가
 export default {
   name : 'CardComponent',
   props : {
