4dd569122d5295e7a67d0a9974b3fad904a79e36
yeoseoyun <yeosummer94@gmail.com>
2024-07-19 20:17:13 +0900

    watch 사용하여 data 감시하기

--- src/ModalComponent.vue
+++ src/ModalComponent.vue
@@ -7,6 +7,7 @@
       <!-- <input>에 입력한 값을 데이터로 저장할 때 "data = $event.target.value"로 사용-->
       <!-- 축약 방법 v-model -->
       <input v-model="month">
+      <!-- <input v-model.number="month"> = 숫자로 저장 -->
       <p> {{ month }}개월 선택 : {{ 원룸들[클릭].price * month }}</p>
       <button @:click="$emit('closeModal')">닫기</button>
       <!-- props로 받아 온 건 read-only -->
@@ -19,6 +20,16 @@
     data() {
       return {
         month : 1,
+        // 문자로 받을때는 초기값을 문자로 설정
+      }
+    },
+    // watch : { 감시할데이터(){}} -> data 감시
+    watch : {
+      // month 데이터가 변할 때마다 watcher도 실행됨
+      month(a) {
+        if (a >= 13) {
+          alert('13이상은 입력하지 마세요')
+        }
       }
     },
     props : {
